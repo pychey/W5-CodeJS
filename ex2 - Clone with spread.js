21db2af77fdad9869545f60b486afb888d6b64cb
Pychey <prakpycheyy@gmail.com>
2025-02-20 14:38:14 +0700

    Complete Exercise-2 (Update Array using Spread Operator)

--- ex2 - Clone with spread.js
+++ ex2 - Clone with spread.js
@@ -14,8 +14,9 @@
 let originalArray = [1, 2, 3, 4, 5];
 
 function updateArray(array, index, newValue) {
-  array[index] = newValue;
-  return array;
+  modifyArray = [...array];
+  modifyArray[index] = newValue;
+  return modifyArray;
 }
 
 let updatedArray = updateArray(originalArray, 2, 10);
